2ec0cf48d46fc55f04eb51aa2e6744b8432e654f
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-09-22 21:35:48 +0330

    the used class name was changed

--- FullTextSearch.Tests/InvertedIndexDictionaryBuilderTest.cs
+++ FullTextSearch.Tests/InvertedIndexDictionaryBuilderTest.cs
@@ -1,39 +1,34 @@
-using System.Net;
 using System.Text.Json;
 using CodeStar2;
 using CodeStar2.Interfaces;
 using FluentAssertions;
 using NSubstitute;
 using Porter2Stemmer;
-using Xunit.Abstractions;
 
 namespace FullTextSearch.Tests;
 
-
-
 public class InvertedIndexDictionaryBuilderTest
 {
+    private readonly Dictionary<string, List<string>>? _expectedInvertedIndex =
+        JsonSerializer.Deserialize<Dictionary<string, List<string>>>(InvertedIndexDictionaryBuilderTestData.InvertedIndexJson);
     
-
-    private readonly IPorter2Stemmer         _stemmer               = Substitute.For<IPorter2Stemmer>();
+    private readonly IPorter2Stemmer         _stemmer                = Substitute.For<IPorter2Stemmer>();
     private readonly IStringToWordsProcessor _stringToWordsProcessor = Substitute.For<IStringToWordsProcessor>();
 
-    
-    private readonly Dictionary<string, List<string>>? _expectedInvertedIndex = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(InvertedIndexTestData.InvertedIndexJson);
-    
 
     [Fact]
     public void TestDifferentTexts()
     {
         //Arrange
         Directory.CreateDirectory("/tmp/unit_test_temp");
-        foreach (TextTestPackage testPackage in InvertedIndexTestData.TestData)
+        foreach (TextTestPackage testPackage in InvertedIndexDictionaryBuilderTestData.TestData)
         {
             File.WriteAllText($"/tmp/unit_test_temp/{testPackage.FileName}", testPackage.Text);
-            
+
             //mock IStringToWordProcessor
             _stringToWordsProcessor.TrimSplitAndStemString(testPackage.Text, _stemmer).Returns(testPackage.Words);
         }
+
         //mock IPorter2Stemmer
         _stemmer.Stem(Arg.Any<string>()).Returns(callInfo =>
         {
@@ -41,14 +36,14 @@ public class InvertedIndexDictionaryBuilderTest
             return new StemmedWord(input, input);
         });
 
-        
+
         var invertedIndexDictionaryBuilder = new InvertedIndexDictionaryBuilder(_stringToWordsProcessor, _stemmer);
 
         //Act
         Dictionary<string, List<string>> invertedIndex = invertedIndexDictionaryBuilder.Build("/tmp/unit_test_temp/");
-        
-        File.WriteAllText("/tmp/unit_test_temp/inv.json",JsonSerializer.Serialize(invertedIndex));
-        
+
+        File.WriteAllText("/tmp/unit_test_temp/inv.json", JsonSerializer.Serialize(invertedIndex));
+
         //Assert
         invertedIndex.Should().BeEquivalentTo(_expectedInvertedIndex);
     }
