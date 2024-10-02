25cf98a7c6d10d6f93f146f1fdae477b19bc5fe4
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-02 08:16:32 +0330

    trunc

--- FullTextSearch/Interfaces/IInvertedIndexDictionaryBuilder.cs
+++
@@ -1,6 +0,0 @@
-namespace FullTextSearch.Interfaces;
-
-public interface IInvertedIndexDictionaryBuilder
-{
-    Dictionary<string, List<string>> Build(string filepath);
-}
--- FullTextSearch/Interfaces/IStringToWordsProcessor.cs
+++
@@ -1,8 +0,0 @@
-using Porter2Stemmer;
-
-namespace FullTextSearch.Interfaces;
-
-public interface IStringToWordsProcessor
-{
-    IEnumerable<string> TrimSplitAndStemString(string source, IPorter2Stemmer stemmer);
-}
