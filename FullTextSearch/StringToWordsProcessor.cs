3621baeb1c1bd5c72957cfcd60dc83b95eb50360
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-07 22:38:49 +0330

    move hardcoded banned words to the AppSettings class.

--- FullTextSearch/StringToWordsProcessor.cs
+++ FullTextSearch/StringToWordsProcessor.cs
@@ -6,19 +6,7 @@ namespace FullTextSearch;
 
 internal partial class StringToWordsProcessor : IStringToWordsProcessor
 {
-    private readonly List<string> _banned =
-    [
-        "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-        "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they",
-        "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those",
-        "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does",
-        "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at",
-        "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above",
-        "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then",
-        "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most",
-        "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t",
-        "can", "will", "just", "don", "should", "now",
-    ];
+    private readonly List<string> _banned = AppSettings.BannedWords.ToList();
     
     [GeneratedRegex(@"(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?")]
     private partial Regex UrlRegex();
