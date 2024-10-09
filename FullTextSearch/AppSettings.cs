2e25864415194cdade424767745701541a12e994
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-09 10:33:23 +0330

    move regex patterns to AppSettings class

--- FullTextSearch/AppSettings.cs
+++ FullTextSearch/AppSettings.cs
@@ -1,6 +1,8 @@
+using System.Text.RegularExpressions;
+
 namespace FullTextSearch;
 
-public static class AppSettings
+internal static partial class AppSettings
 {
     public const string DocumentsPath = "/home/shafagh/Desktop/EnglishData";
 
@@ -21,4 +23,17 @@ public static class AppSettings
     public static readonly char[] TrimableSpecialCharacters = ":.,<>?/\\\"\t'`~!@#$%^&*()_+=-*;\t |".ToArray();
     
     public static readonly char[] SplitterSpecialCharacters = "-_'()[]'\";:/,\\><=".ToArray();
+
+    internal static partial class RegexPatterns
+    {
+        
+        [GeneratedRegex(@"(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?")]
+        public static partial Regex UrlRegex();
+
+        [GeneratedRegex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")]
+        public static partial Regex EmailRegex();
+
+        [GeneratedRegex(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")]
+        public static partial Regex PhoneNumberRegex();
+    }
 }
--- FullTextSearch/Application/InvertedIndex/StringToWordsProcessor.cs
+++ FullTextSearch/Application/InvertedIndex/StringToWordsProcessor.cs
@@ -9,12 +9,6 @@ internal partial class StringToWordsProcessor : IStringToWordsProcessor
     private readonly IPorter2Stemmer _stemmer;
     private readonly List<string>    _banned = AppSettings.BannedWords.ToList();
     
-    [GeneratedRegex(@"(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?")]
-    private partial Regex UrlRegex();
-    [GeneratedRegex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")]
-    private partial Regex EmailRegex();
-    [GeneratedRegex(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")]
-    private partial Regex PhoneNumberRegex();
 
     
     
@@ -42,10 +36,12 @@ internal partial class StringToWordsProcessor : IStringToWordsProcessor
     private bool IsNotBanned(string value) => !_banned.Contains(value);
 
     private static bool IsLongEnough(string value) => value.Length >= 3;
-    
-    
-    private bool IsNotNoise(string value) =>
-        !UrlRegex().IsMatch(value) && !EmailRegex().IsMatch(value) && !PhoneNumberRegex().IsMatch(value);
+
+
+    private static bool IsNotNoise(string value) =>
+        !AppSettings.RegexPatterns.UrlRegex().IsMatch(value) &&
+        !AppSettings.RegexPatterns.EmailRegex().IsMatch(value) &&
+        !AppSettings.RegexPatterns.PhoneNumberRegex().IsMatch(value);
 
     private string Stem(string value) => _stemmer.Stem(value).Value.ToLower();
     
