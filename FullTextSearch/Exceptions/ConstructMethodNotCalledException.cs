f202b07b9a09783240ff42c287a18024bc403f55
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-13 10:08:42 +0330

    feat(exceptions): implement public constructors of each exception for handling of exceptions.

--- FullTextSearch/Exceptions/ConstructMethodNotCalledException.cs
+++ FullTextSearch/Exceptions/ConstructMethodNotCalledException.cs
@@ -2,5 +2,19 @@ namespace FullTextSearch.Exceptions;
 
 public class ConstructMethodNotCalledException : Exception
 {
-    public override string Message => "The Construct Method is not called";
+
+    public ConstructMethodNotCalledException() : base("The Construct Method is not called") //Default message
+    {
+        
+    }
+
+    public ConstructMethodNotCalledException(string message) : base(message)
+    {
+        
+    }
+
+    public ConstructMethodNotCalledException(string message, Exception innerException) : base(message, innerException)
+    {
+        
+    }
 }
--- FullTextSearch/Exceptions/NullInputException.cs
+++ FullTextSearch/Exceptions/NullInputException.cs
@@ -2,5 +2,19 @@ namespace FullTextSearch.Exceptions;
 
 public class NullInputException: Exception
 {
-    public override string Message => "The input text is null or white spaces";
+    
+    public NullInputException() : base("The input text is null or white spaces") //Default message
+    {
+        
+    }
+
+    public NullInputException(string message) : base(message)
+    {
+        
+    }
+
+    public NullInputException(string message, Exception innerException) : base(message, innerException)
+    {
+        
+    }
 }
