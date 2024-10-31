a9ea13ad96e664fc2ebd3baeedfafaabad756e6a
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-31 19:39:11 +0330

    test(PrefixBasedAndWordsProcessor): add tests

---
+++ FullTextSearch.Tests/Application/WordProcessors/PrefixBasedAndWordsProcessorTests.cs
@@ -0,0 +1,49 @@
+using FluentAssertions;
+using FullTextSearch.Application.WordsProcessors.Services;
+using NSubstitute;
+using Porter2Stemmer;
+
+namespace FullTextSearch.Tests.Application.WordProcessors;
+
+public class PrefixBasedAndWordsProcessorTests
+{
+    private readonly PrefixBasedAndWordsProcessor _wordsProcessor;
+
+    public PrefixBasedAndWordsProcessorTests()
+    {
+        //Arrange
+
+        var stemmer = Substitute.For<IPorter2Stemmer>();
+
+        //returns input without any change
+        stemmer.Stem(Arg.Any<string>()).Returns(callInfo =>
+        {
+            var input = callInfo.Arg<string>();
+            return new(input, input);
+        });
+
+        _wordsProcessor = new(stemmer);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GetAndDocuments_returnsIntersectedDocuments(string[] queryWords, IReadOnlyList<string> expectedResult)
+    {
+        //Act
+        var result = _wordsProcessor.GetAndWords(queryWords);
+
+        //Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+
+    public static IEnumerable<object?[]> TestData()
+    {
+        yield return [new[] { "word1", "word2", }, new List<string> { "word1", "word2", },];
+        yield return [new[] { "word1", "+word2", }, new List<string> { "word1", },];
+        yield return [new[] { "word1", "-word2", }, new List<string> { "word1", },];
+        yield return [new[] { "+word1", "-word2", }, new List<string>(),];
+        yield return [new[] { "word1", "word2", "+word3", "+word4", "-word5", }, new List<string>{"word1", "word2", },];
+        yield return [Array.Empty<string>(), new List<string>(),];
+    }
+}
