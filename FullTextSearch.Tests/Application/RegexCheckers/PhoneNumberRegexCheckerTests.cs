957d522b006123936a72804c0014b7199b5945b6
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-31 15:53:15 +0330

    test(PhoneNumberRegexCheckerTests): remove space around phone number tests as it's not needed

--- FullTextSearch.Tests/Application/RegexCheckers/PhoneNumberRegexCheckerTests.cs
+++ FullTextSearch.Tests/Application/RegexCheckers/PhoneNumberRegexCheckerTests.cs
@@ -21,17 +21,17 @@ public class PhoneNumberRegexCheckerTests
 
     public static IEnumerable<object?[]> TrueTestData()
     {
-        yield return [" +1 234-567-8900", true,];
-        yield return [" +44 20 1234 5678", true,];
-        yield return [" +91 98765 43210", true,];
-        yield return [" +49 30 12345678", true,];
+        yield return ["+1 234-567-8900", true,];
+        yield return ["+44 20 1234 5678", true,];
+        yield return ["+91 98765 43210", true,];
+        yield return ["+49 30 12345678", true,];
         yield return ["+61 2 1234 5678", true,];
-        yield return ["123-456-7890 ", true,];
-        yield return ["123.456.7890 ", true,];
+        yield return ["123-456-7890", true,];
+        yield return ["123.456.7890", true,];
         yield return ["1234567890", true,];
         yield return ["123 456 7890", true,];
         yield return ["020 1234 5678", true,];
-        yield return ["0121 123 4567 words", true,];
+        yield return ["0121 123 4567", true,];
         yield return ["07700 900123", true,];
         yield return ["07890 123456", true,];
         yield return ["+33 1 23 45 67 89", true,];
