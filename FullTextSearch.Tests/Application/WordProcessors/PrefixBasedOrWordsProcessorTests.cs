dbd13915db4e7435184cc1e24355a023ba2922fa
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-31 19:49:53 +0330

    test(PrefixBasedOrWordsProcessor): add tests

---
+++ FullTextSearch.Tests/Application/WordProcessors/PrefixBasedOrWordsProcessorTests.cs
@@ -0,0 +1,60 @@
+using FluentAssertions;
+using FullTextSearch.Application.WordsProcessors.Services;
+using NSubstitute;
+using Porter2Stemmer;
+
+namespace FullTextSearch.Tests.Application.WordProcessors;
+
+public class PrefixBasedOrWordsProcessorTests
+{
+    private readonly PrefixBasedOrWordsProcessor _wordsProcessor;
+
+    public PrefixBasedOrWordsProcessorTests()
+    {
+        //Arrange
+
+        var stemmer = Substitute.For<IPorter2Stemmer>();
+
+        //returns input without any change
+        stemmer.Stem(Arg.Any<string>()).Returns(callInfo =>
+        {
+            var input = callInfo.Arg<string>();
+            return new(input, input);
+        });
+
+        _wordsProcessor = new(stemmer);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GetOrDocuments_returnsIntersectedDocuments(string[] queryWords, IReadOnlyList<string> expectedResult)
+    {
+        //Act
+        var result = _wordsProcessor.GetOrWords(queryWords);
+
+        //Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+
+    public static IEnumerable<object?[]> TestData()
+    {
+        yield return [new[] { "+word1", "+word2", }, new List<string> { "word1", "word2", },];
+        yield return [new[] { "word1", "+word2", }, new List<string> { "word2", },];
+        yield return [new[] { "word1", "-word2", }, new List<string> {  },];
+        yield return [new[] { "+word1", "-word2", }, new List<string>{"word1"},];
+        yield return [new[] { "word1", "word2", "+word3", "+word4", "-word5", }, new List<string>{"word3", "word4", },];
+        yield return [Array.Empty<string>(), new List<string>(),];
+    }
+    
+    [Fact]
+    public void Constructor_ShouldThrowArgumentNullException_WhenDependenciesAreNull()
+    {
+        // Arrange
+        IPorter2Stemmer stemmer = null!;
+
+        // Act & Assert
+        Action act = () => new PrefixBasedOrWordsProcessor(stemmer);
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
