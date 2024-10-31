cf127a3d7beee979bd4b9529e0959cbfab6bec00
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-31 10:25:14 +0330

    test(NotDocumentsReader): add tests to check union operations performed in it

---
+++ FullTextSearch.Tests/DocumentsReader/NotDocumentsReaderTests.cs
@@ -0,0 +1,47 @@
+using FluentAssertions;
+using FullTextSearch.Application.DocumentsReader.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FullTextSearch.Tests.DocumentsReader;
+
+public class NotDocumentsReaderTests
+{
+    private readonly Dictionary<string, List<string>> _invertedIndex = new()
+    {
+        { "green", ["1", "2", "3",] },
+        { "blue", ["1", "2",] },
+        { "red", ["1", "2",] },
+        { "orange", ["2", "4", "5",] },
+        { "yellow", ["4", "5", "6",] },
+        { "brown", ["7", "8", "9",] },
+        { "purple", ["2", "10", "11",] },
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Tests(IReadOnlyList<string> andWords, HashSet<string> expectedResult)
+    {
+        //Arrange
+        var reader = ServiceCollection.ServiceProvider.GetService<INotDocumentsReader>();
+
+        //pre Assert
+        reader.Should().NotBeNull();
+
+        //Act
+        var result = reader!.GetNotDocuments(_invertedIndex, andWords);
+
+        //Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    public static IEnumerable<object?[]> TestData()
+    {
+        yield return [new[] { "green", "blue", }, new HashSet<string> { "1", "2", "3", },];
+        yield return [new[] { "red", "blue", }, new HashSet<string> { "1", "2", },];
+        yield return [new[] { "green", "orange", }, new HashSet<string> { "1", "2", "3", "4", "5", },];
+        yield return [new[] { "green", "black", }, new HashSet<string> { "1", "2", "3", },];
+        yield return [new[] { "green", "yellow", "brown", }, new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", },];
+        yield return [new[] { "green", "orange", "purple", }, new HashSet<string> { "1", "2", "3", "4", "5", "10", "11", },];
+        yield return [new[] { "green", "orange", "purple", "brown", }, new HashSet<string> { "1", "2", "3", "4", "5", "7", "8", "9", "10", "11", },];
+    }
+}
