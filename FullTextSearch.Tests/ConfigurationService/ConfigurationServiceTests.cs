b9baa740b7db5a20d39eda55638cd04822697881
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-10-31 09:00:21 +0330

    test(ConfigurationService): add new test
    Should Contain DocumentsPath.

--- FullTextSearch.Tests/ConfigurationService/ConfigurationServiceTests.cs
+++ FullTextSearch.Tests/ConfigurationService/ConfigurationServiceTests.cs
@@ -7,9 +7,17 @@ namespace FullTextSearch.Tests.ConfigurationService;
 public class ConfigurationServiceTests
 {
     private readonly IConfigurationService? _configurationService = ServiceCollection.ServiceProvider.GetService<IConfigurationService>();
+    
     [Fact]
     public void NotNull()
     {
         _configurationService.Should().NotBeNull();
     }
+
+    [Fact]
+    public void DocumentsPathNotNull()
+    {
+        _configurationService!.GetConfig()["DocumentsPath"].Should().NotBeNull();
+    }
+    
 }
