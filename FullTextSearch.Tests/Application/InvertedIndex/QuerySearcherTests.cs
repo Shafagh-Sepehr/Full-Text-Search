49d33411d0eab5920bd77cc985b3fed9c5e0ee8d
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-11-07 19:40:55 +0330

    test(QuerySearcherTests): fix Search_ReturnsList logic which was wrong
    Search_ReturnsList's mocking wasn't correct and the mocked methods were actually called with different inputs resulting in null returns, which would pass the test due to a wrong assertion.

--- FullTextSearch.Tests/Application/InvertedIndex/QuerySearcherTests.cs
+++ FullTextSearch.Tests/Application/InvertedIndex/QuerySearcherTests.cs
@@ -74,8 +74,8 @@ public class QuerySearcherTests
     public void Search_ReturnsList()
     {
         //Arrange
-        var query = "  query words are here ";
-        var processedQuery = "  query words are here ".Trim().Split();
+        const string query = "  query words are here ";
+        var processedQuery = query.Trim().Split();
         var invertedIndex = new Dictionary<string, List<string>>
         {
             { "key1", ["value1", "value2",] },
@@ -83,37 +83,38 @@ public class QuerySearcherTests
             { "key3", ["value5", "value6",] },
         };
 
+        var andReturns = new List<string> { "key1", "keey1", };
+        var orReturns = new List<string> { "key2", "keey2", };
+        var notReturns = new List<string> { "key3", "keey3", };
+
         _querySearcher.Construct(invertedIndex);
 
-        _wordsProcessor.GetOrWords(processedQuery).Returns(["key1", "keey1",]);
-        _wordsProcessor.GetAndWords(processedQuery).Returns(["key2", "keey2",]);
-        _wordsProcessor.GetNotWords(processedQuery).Returns(["key3", "keey3",]);
+        _wordsProcessor.GetOrWords(Arg.Is<string[]>(x => x.Length == processedQuery.Length && !x.Except(processedQuery).Any())).Returns(orReturns);
+        _wordsProcessor.GetAndWords(Arg.Is<string[]>(x => x.Length == processedQuery.Length && !x.Except(processedQuery).Any())).Returns(andReturns);
+        _wordsProcessor.GetNotWords(Arg.Is<string[]>(x => x.Length == processedQuery.Length && !x.Except(processedQuery).Any())).Returns(notReturns);
 
         var expectedResult = new HashSet<string> { "res1", "res2", };
         var expectedResultCopy = new HashSet<string>(expectedResult);
 
-        var processedWords = new ProcessedQueryWords
-        {
-            OrWords = _wordsProcessor.GetOrWords(processedQuery),
-            AndWords = _wordsProcessor.GetAndWords(processedQuery),
-            NotWords = _wordsProcessor.GetNotWords(processedQuery),
-        };
 
-        _searchExecutor.ExecuteSearch(processedWords).Returns(expectedResult);
+        _searchExecutor.ExecuteSearch(Arg.Is<ProcessedQueryWords>(x =>
+            x.AndWords.Count == andReturns.Count && !x.AndWords.Except(andReturns).Any() &&
+            x.OrWords.Count == orReturns.Count && !x.OrWords.Except(orReturns).Any() &&
+            x.NotWords.Count == notReturns.Count && !x.NotWords.Except(notReturns).Any())).Returns(expectedResult);
 
         //Act
         var result = _querySearcher.Search(query);
 
         //Assert
-        result.Should().BeEmpty();
-        _wordsProcessor.Received(1).GetOrWords(processedQuery);
-        _wordsProcessor.Received(1).GetAndWords(processedQuery);
-        _wordsProcessor.Received(1).GetNotWords(processedQuery);
+        result.Should().BeEquivalentTo(expectedResult);
+        _wordsProcessor.Received(1).GetOrWords(Arg.Is<string[]>(x => x.Length == processedQuery.Length && !x.Except(processedQuery).Any()));
+        _wordsProcessor.Received(1).GetAndWords(Arg.Is<string[]>(x => x.Length == processedQuery.Length && !x.Except(processedQuery).Any()));
+        _wordsProcessor.Received(1).GetNotWords(Arg.Is<string[]>(x => x.Length == processedQuery.Length && !x.Except(processedQuery).Any()));
         _searchExecutor.Received(1).ExecuteSearch(Arg.Do<ProcessedQueryWords>(processedQueryWords =>
         {
-            processedQueryWords.OrWords.Should().BeEquivalentTo(processedWords.OrWords);
-            processedQueryWords.AndWords.Should().BeEquivalentTo(processedWords.AndWords);
-            processedQueryWords.NotWords.Should().BeEquivalentTo(processedWords.NotWords);
+            processedQueryWords.OrWords.Should().BeEquivalentTo(orReturns);
+            processedQueryWords.AndWords.Should().BeEquivalentTo(andReturns);
+            processedQueryWords.NotWords.Should().BeEquivalentTo(notReturns);
         }));
         expectedResult.Should().BeEquivalentTo(expectedResultCopy);
     }
