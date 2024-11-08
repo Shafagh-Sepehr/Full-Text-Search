f8127422daac90ae359a55de5ff54f70d3213cfd
Shafagh-Sepehr <shafaghsepehrofficial@gmail.com>
2024-11-08 11:19:04 +0330

    test(SearchExecutorTests): remove redundant assertions
    remove redundant assertions that were already covered by previous assertions.

--- FullTextSearch.Tests/Application/InvertedIndex/SearchExecutorTests.cs
+++ FullTextSearch.Tests/Application/InvertedIndex/SearchExecutorTests.cs
@@ -58,11 +58,8 @@ public class SearchExecutorTests
         var result = _searchExecutor.ExecuteSearch(words);
 
         // Assert
-        result.Should().BeEquivalentTo(expected);
+        result.Should().BeSameAs(expected);
         _searcher.Received(1).AndOrNotSearch(invertedIndex, words);
-
-        expected.Should().BeEquivalentTo(_originalExpectedResult);
-        invertedIndex.Should().BeEquivalentTo(_originalInvertedIndex);
     }
 
     [Fact]
@@ -84,11 +81,8 @@ public class SearchExecutorTests
         var result = _searchExecutor.ExecuteSearch(words);
 
         // Assert
-        result.Should().BeEquivalentTo(expected);
+        result.Should().BeSameAs(expected);
         _searcher.Received(1).AndNotSearch(invertedIndex, words);
-
-        expected.Should().BeEquivalentTo(_originalExpectedResult);
-        invertedIndex.Should().BeEquivalentTo(_originalInvertedIndex);
     }
 
     [Fact]
@@ -110,11 +104,8 @@ public class SearchExecutorTests
         var result = _searchExecutor.ExecuteSearch(words);
 
         // Assert
-        result.Should().BeEquivalentTo(expected);
+        result.Should().BeSameAs(expected);
         _searcher.Received(1).OrNotSearch(invertedIndex, words);
-
-        expected.Should().BeEquivalentTo(_originalExpectedResult);
-        invertedIndex.Should().BeEquivalentTo(_originalInvertedIndex);
     }
 
     [Fact]
@@ -138,7 +129,6 @@ public class SearchExecutorTests
         _searcher.Received(0).AndNotSearch(Arg.Any<Dictionary<string, List<string>>>(), Arg.Any<ProcessedQueryWords>());
         _searcher.Received(0).OrNotSearch(Arg.Any<Dictionary<string, List<string>>>(), Arg.Any<ProcessedQueryWords>());
         result.Should().BeEmpty();
-        invertedIndex.Should().BeEquivalentTo(_originalInvertedIndex);
     }
 
     [Fact]
